1412444500a52ca132692d2c280a7b72ed060f8c
Ryann Rourk <ryannrourk@gmail.com>
2021-03-02 10:13:16 -0500

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,11 @@
+// WEATHER APP REACT FILE
+
+function App() {
+
+    return (
+        <React.Fragment>
+            {/* SEARCH BAR */}
+        </React.Fragment>
+    )
+
+}
---
+++ server.py
@@ -0,0 +1,16 @@
+# SERVER
+# WEATHER APP PROJECT
+
+
+
+# API: OpenWeather https://openweathermap.org/api
+
+# Build a weather app that displays a 5-day weather forecast.
+
+# Include city name, current weather icon, temp, humidity, wind speed, etc
+
+# Display the recording of both high and low temps of each day
+# Display images for sunny/rainy/cloudy/snowy weather conditions
+# Refresh every 5 minutes with exact temp & weather conditions
+
+# use font awesome for icons
